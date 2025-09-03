8156c89b218978b8b95ab9d691ad64818aa10ecc
Douglas.Y <siyuan.yang2@uq.net.au>
2025-09-03 17:13:31 +1000

    test: AWS work well with this version

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,2 +1,5 @@
 import serverlessExpress from "@vendia/serverless-express";
-import app from "./app";
+import app from "./app.js";
+
+// TS sees the imported module as a namespace; cast to any to call it without changing logic
+export const handler = (serverlessExpress as any)({ app });
